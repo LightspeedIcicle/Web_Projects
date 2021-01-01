aeb5d142ad555f4e5489a6a83300af5275e5b4c1
Michael Oh <i.am@michael-oh.dev>
2020-12-31 19:24:11 -0800

    Initial Commit

---
+++ WallowaLakeTramway/WallowaLakeTramway/WallowaLakeTramway/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for WallowaLakeTramway project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WallowaLakeTramway.settings')
+
+application = get_asgi_application()
